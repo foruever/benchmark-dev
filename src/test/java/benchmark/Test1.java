112d4ca1b7ceb214a310403d6979f13a221d66c9
stepten <wstepten@163.com>
2017-06-27 20:21:35 +0800

    dbutils

---
+++ src/test/java/benchmark/Test1.java
@@ -0,0 +1,9 @@
+package benchmark;
+
+import org.apache.commons.dbutils.QueryRunner;
+
+public class Test1 {
+	public static void main(String[] args) {
+		QueryRunner runner=new QueryRunner();
+	}
+}
